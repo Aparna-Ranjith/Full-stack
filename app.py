13a3f0eb4707371076faa31009e4f73b87533cd5
Aparna-Ranjith <aparna.paru93@gmail.com>
2020-08-08 11:22:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__) #creating a Flask class object
   
 @app.route('/')
 def hello():  
-    return "Hello World!";
+    return ("Hello World!")
+
      
 if __name__ =='__main__':  
-    app.run(debug = True)
+    app.run()
