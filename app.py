570864dbdf3d7126968e95c918ded6469357b1d7
Aparna-Ranjith <aparna.paru93@gmail.com>
2020-08-09 15:31:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "Welcome back Aparna!!"
     
 if __name__ == "__main__":
     app.run(debug=True)
